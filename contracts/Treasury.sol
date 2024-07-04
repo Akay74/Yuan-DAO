d7f26b3266e7c1d477abce206908903ea8a2936f
Akay74 <akaudeh@gmail.com>
2024-07-04 21:40:37 +0100

    💡 add comments for Treasury contract

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -15,25 +15,45 @@ contract Treasury is ITreasury, Ownable, VotesCounter {
     
     uint8 private _decimals = 8;
 
-    mapping(bytes32 => mapping(uint256 => address)) proposalWinner;
+    mapping(bytes32 => mapping(uint256 => address)) private _proposalWinner;
 
     constructor(address _governanceToken) Ownable() {
         require(_governanceToken != address(0), "Governance token cannot be zero address");
         governanceToken = _governanceToken;
     }
 
+    /**
+     * @dev See {ITreasury-setWinningToken}.
+     */
     function setWinningToken(bytes32 proposalId, uint8 option, address winningToken) external onlyOwner {
         _setWinningToken(proposalId, option, winningToken);
     }
 
+    /**
+     * @dev See {ITreasury-getTokenBalance}.
+     */
     function getTokenBalance() public view returns(uint256) {
         return IERC20(governanceToken).balanceOf(address(this));
     }
 
+    /**
+     * @dev See {ITreasury-getWinningToken}.
+     */
     function getWinningToken(bytes32 proposalId, uint8 option) public view returns(address) {
-        return proposalWinner[proposalId][option];
+        return _proposalWinner[proposalId][option];
     }
 
+    /**
+     * @dev Transfers vote tokens from the sender to this contract.
+     * @param amount The amount of tokens to transfer (before applying decimals).
+     *
+     * Requirements:
+     * - The sender must have sufficient balance.
+     * - The transfer must be approved.
+     * - The amount must be greater than zero.
+     *
+     * Emits a {Transfer} event.
+     */
     function _transferVoteToken(uint256 amount) internal {
         uint256 amountToTransfer = amount * 10**_decimals;
         uint256 senderBalance = IERC20(governanceToken).balanceOf(msg.sender);
@@ -45,10 +65,28 @@ contract Treasury is ITreasury, Ownable, VotesCounter {
         IERC20(governanceToken).safeTransferFrom(msg.sender, address(this), amountToTransfer);
     }
 
+    /**
+     * @dev Sets the winning token for a specific proposal and option.
+     * @param proposalId The unique identifier of the proposal.
+     * @param option The option number.
+     * @param winningToken The address of the winning token.
+     *
+     * Note: This function does not emit an event. Consider adding an event
+     * if tracking these changes on-chain is important for your use case.
+     */
     function _setWinningToken(bytes32 proposalId, uint8 option, address winningToken) internal {
-        proposalWinner[proposalId][option] = winningToken;
+        _proposalWinner[proposalId][option] = winningToken;
     }
     
+    /**
+     * @dev Burns a specified amount of governance tokens from this contract.
+     * @param amount The amount of tokens to burn (before applying decimals).
+     *
+     * Requirements:
+     * - The contract must have sufficient balance of governance tokens.
+     *
+     * Emits a {Transfer} event.
+     */
     function _burn(uint256 amount) internal {
         uint256 amountToBurn = amount * 10**_decimals;
         uint256 governanceTokenBalance = IERC20(governanceToken).balanceOf(address(this));
