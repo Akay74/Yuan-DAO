bafc4bc6858f2bcf7942f2a517960ff8980dbfd5
Akay74 <akaudeh@gmail.com>
2024-09-14 13:17:42 +0100

    ✅ refactor test cases

--- test/treasury.test.js
+++ test/treasury.test.js
@@ -11,21 +11,21 @@ describe("Treasury", function () {
     // Deploy mock GovernanceToken
     GovernanceToken = await ethers.getContractFactory("MockERC20");
     governanceToken = await GovernanceToken.deploy("GovernanceToken", "GT", 8);
-    await governanceToken.deployed();
+    await governanceToken.waitForDeployment();
 
     // Deploy Treasury contract
     Treasury = await ethers.getContractFactory("Treasury");
-    treasury = await Treasury.deploy(governanceToken.address);
-    await treasury.deployed();
+    treasury = await Treasury.deploy(await governanceToken.getAddress());
+    await treasury.waitForDeployment();
 
     // Mint some tokens for testing
-    await governanceToken.mint(owner.address, ethers.utils.parseUnits("1000", 8));
-    await governanceToken.mint(addr1.address, ethers.utils.parseUnits("1000", 8));
+    await governanceToken.mint(owner.address, ethers.parseUnits("1000", 8));
+    await governanceToken.mint(addr1.address, ethers.parseUnits("1000", 8));
   });
 
   describe("Deployment", function () {
     it("Should set the correct governance token", async function () {
-      expect(await treasury.governanceToken()).to.equal(governanceToken.address);
+      expect(await treasury.governanceToken()).to.equal(await governanceToken.getAddress());
     });
 
     it("Should set the correct owner", async function () {
@@ -35,7 +35,7 @@ describe("Treasury", function () {
 
   describe("setWinningToken", function () {
     it("Should set the winning token correctly", async function () {
-      const proposalId = ethers.utils.id("proposal1");
+      const proposalId = ethers.id("proposal1");
       const option = 1;
       const winningToken = addr2.address;
 
@@ -47,7 +47,7 @@ describe("Treasury", function () {
     });
 
     it("Should revert when called by non-owner", async function () {
-      const proposalId = ethers.utils.id("proposal1");
+      const proposalId = ethers.id("proposal1");
       const option = 1;
       const winningToken = addr2.address;
 
@@ -59,26 +59,29 @@ describe("Treasury", function () {
 
   describe("burnGovernanceToken", function () {
     it("Should burn governance tokens correctly", async function () {
-      const amountToBurn = ethers.utils.parseUnits("100", 8);
+      const amountToBurn = ethers.parseUnits("100", 8);
       
       // Transfer tokens to the treasury
-      await governanceToken.transfer(treasury.address, amountToBurn);
+      await governanceToken.transfer(await treasury.getAddress(), amountToBurn);
+      
+      // Approve treasury to burn tokens
+      await governanceToken.connect(owner).approve(await treasury.getAddress(), amountToBurn);
 
       await expect(treasury.burnGovernanceToken(100))
         .to.emit(treasury, "TokenBurned")
         .withArgs(100);
 
-      const treasuryBalance = await governanceToken.balanceOf(treasury.address);
+      const treasuryBalance = await governanceToken.balanceOf(await treasury.getAddress());
       expect(treasuryBalance).to.equal(0);
     });
 
     it("Should revert when burning more tokens than available", async function () {
-      const amountToBurn = ethers.utils.parseUnits("100", 8);
+      const amountToBurn = ethers.parseUnits("100", 8);
       
       // Transfer tokens to the treasury
-      await governanceToken.transfer(treasury.address, amountToBurn);
+      await governanceToken.transfer(await treasury.getAddress(), amountToBurn);
 
-      await expect(treasury.burnGovernanceToken(101)).to.be.revertedWith("InsufficientFunds");
+      await expect(treasury.burnGovernanceToken(101)).to.be.revertedWithCustomError(treasury, "InsufficientFunds");
     });
 
     it("Should revert when called by non-owner", async function () {
@@ -88,36 +91,36 @@ describe("Treasury", function () {
 
   describe("transferVoteToken", function () {
     it("Should transfer vote tokens correctly", async function () {
-      const amountToTransfer = ethers.utils.parseUnits("100", 8);
+      const amountToTransfer = ethers.parseUnits("100", 8);
       
-      await governanceToken.connect(addr1).approve(treasury.address, amountToTransfer);
+      await governanceToken.connect(addr1).approve(await treasury.getAddress(), amountToTransfer);
       
       await expect(treasury.connect(addr1).transferVoteToken(100))
         .to.emit(governanceToken, "Transfer")
-        .withArgs(addr1.address, treasury.address, amountToTransfer);
+        .withArgs(addr1.address, await treasury.getAddress(), amountToTransfer);
 
-      const treasuryBalance = await governanceToken.balanceOf(treasury.address);
+      const treasuryBalance = await governanceToken.balanceOf(await treasury.getAddress());
       expect(treasuryBalance).to.equal(amountToTransfer);
     });
 
     it("Should revert when transferring more tokens than available", async function () {
-      const amountToTransfer = ethers.utils.parseUnits("1001", 8);
+      const amountToTransfer = ethers.parseUnits("1001", 8);
       
-      await governanceToken.connect(addr1).approve(treasury.address, amountToTransfer);
+      await governanceToken.connect(addr1).approve(await treasury.getAddress(), amountToTransfer);
       
-      await expect(treasury.connect(addr1).transferVoteToken(1001)).to.be.revertedWith("InsufficientFunds");
+      await expect(treasury.connect(addr1).transferVoteToken(1001)).to.be.revertedWithCustomError(treasury, "InsufficientFunds");
     });
 
-    it("Should revert when approval fails", async function () {
-      await expect(treasury.connect(addr1).transferVoteToken(100)).to.be.revertedWith("ApprovalFailed");
+    it("Should revert when approval is not given", async function () {
+      await expect(treasury.connect(addr1).transferVoteToken(100)).to.be.revertedWith("ERC20: insufficient allowance");
     });
   });
 
   describe("getTokenBalance", function () {
     it("Should return the correct token balance", async function () {
-      const amountToTransfer = ethers.utils.parseUnits("100", 8);
+      const amountToTransfer = ethers.parseUnits("100", 8);
       
-      await governanceToken.transfer(treasury.address, amountToTransfer);
+      await governanceToken.transfer(await treasury.getAddress(), amountToTransfer);
 
       const balance = await treasury.getTokenBalance();
       expect(balance).to.equal(amountToTransfer);
