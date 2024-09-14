c65f654df84de9b9e05a7ec3599dfcdbd3574bcb
Akay74 <akaudeh@gmail.com>
2024-09-14 12:21:03 +0100

    🎨 refactor contract

--- contracts/Mock/MockERC20.sol
+++ contracts/Mock/MockERC20.sol
@@ -1,24 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
+pragma solidity ^0.8.17;
 
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-/**
- * @title The test ERC20
- * @author Polytrade.Finance
- */
 contract MockERC20 is ERC20 {
-    uint8 private immutable _decimals;
+    uint8 private _decimals;
 
-    constructor(
-        string memory name_,
-        string memory symbol_,
-        uint8 decimals_,
-        address receiver_,
-        uint256 totalSupply_
-    ) ERC20(name_, symbol_) {
+    constructor(string memory name, string memory symbol, uint8 decimals_) ERC20(name, symbol) {
         _decimals = decimals_;
-        _mint(receiver_, totalSupply_ * (10 ** decimals_));
+    }
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
     }
 
     function decimals() public view virtual override returns (uint8) {
