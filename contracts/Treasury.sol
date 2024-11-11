c9482996b765920760320b8865f588cb847f04e8
Akay74 <akaudeh@gmail.com>
2024-11-11 23:04:32 +0100

    🎨 refactor burn function

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -1,25 +1,29 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.17;
 
 import { ITreasury } from "./interfaces/ITreasury.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { VotesCounter } from "./VotesCounter.sol"; // remove and replace with the address of the deployed contract
+import { VotesCounter } from "./VotesCounter.sol";
+
+interface IERC20Burnable is IERC20 {
+    function burn(uint256 amount) external;
+}
 
 contract Treasury is ITreasury, Ownable, VotesCounter {
     using SafeERC20 for IERC20;
 
     address public governanceToken;
-
-    uint8 private _decimals = 8; //create a method to update the decimal in case the governance token is updated
+    uint8 private _decimals;
 
     mapping(bytes32 => mapping(uint256 => address)) private _proposalWinner;
 
     constructor(address _governanceToken) Ownable() {
         if (_governanceToken == address(0)) revert ZeroAddress();
         governanceToken = _governanceToken;
+        _decimals = IERC20Metadata(_governanceToken).decimals();
     }
 
     /**
@@ -70,16 +74,12 @@ contract Treasury is ITreasury, Ownable, VotesCounter {
      * Emits a {Transfer} event.
      */
     function _transferVoteToken(uint256 amount) internal {
+        if (amount == 0) revert ApprovalFailed();
         uint256 amountToTransfer = amount * 10 ** _decimals;
         uint256 senderBalance = IERC20(governanceToken).balanceOf(msg.sender);
 
         if (senderBalance < amountToTransfer) revert InsufficientFunds();
-        bool approved = IERC20(governanceToken).approve(
-            address(this),
-            amountToTransfer
-        );
-
-        if (!approved || amount == 0) revert ApprovalFailed();
+        
         IERC20(governanceToken).safeTransferFrom(
             msg.sender,
             address(this),
@@ -104,25 +104,26 @@ contract Treasury is ITreasury, Ownable, VotesCounter {
 
     /**
      * @dev Burns a specified amount of governance tokens from this contract.
-     * @param amount The amount of tokens to burn (before applying decimals).
+     * @param amount The amount of tokens to burn (in token units, not wei).
      *
      * Requirements:
      * - The contract must have sufficient balance of governance tokens.
      *
-     * Emits a {Transfer} event.
+     * Emits a {TokenBurned} event.
      */
     function _burn(uint256 amount) internal {
-        uint256 amountToBurn = amount * 10 ** _decimals;
-        uint256 governanceTokenBalance = IERC20(governanceToken).balanceOf(
-            address(this)
-        );
+        uint256 amountToBurn = amount * (10 ** _decimals);
+        uint256 governanceTokenBalance = IERC20(governanceToken).balanceOf(address(this));
         if (governanceTokenBalance < amountToBurn) revert InsufficientFunds();
-        emit TokenBurned(amount);
 
-        IERC20(governanceToken).safeTransferFrom(
-            address(this),
-            address(0),
-            amountToBurn
-        );
+        // Try to use the burn function if it exists
+        try IERC20Burnable(governanceToken).burn(amountToBurn) {
+            // Burning successful
+        } catch {
+            // If burn function doesn't exist or fails, transfer to zero address
+            IERC20(governanceToken).transfer(address(0), amountToBurn);
+        }
+
+        emit TokenBurned(amountToBurn);
     }
 }
